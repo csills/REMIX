3477115baaa26c607061e687bfb3eb225cd490ac
csills <sillscrystal@gmail.com>
2018-07-27 19:34:51 -0400

    added temporary logo and tag line

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 //import Axios from 'axios';
 //import { Link, Route } from 'react-router-dom';
 import './App.css';
+import remixLogo from './Remix-Logo.png';
 import Gallery from './Gallery';
 import './Gallery.css';
-import './Remix-Logo.png';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      collab: "A space to create and re-create."
+      remixTagLine: "A space to create and re-create."
     }
   }
 
@@ -21,9 +21,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="Remix-Logo.png" className="App-logo" alt="logo" />
+          <img src={remixLogo} className="App-logo" alt="logo" />
         </header>
-        <h2>{this.state.collab}</h2>
+        <h2>{this.state.remixTagLine}</h2>
         <p className="App-intro">
          "Remix culture, sometimes read-write culture, is a society that allows and encourages derivative works by combining or editing existing materials to produce a new creative work or product."
         </p>
