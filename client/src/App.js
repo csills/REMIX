410ad17d050616e7837253fab989584d2dbe5a44
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2018-07-29 03:56:31 -0400

    added email register login download

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import remixLogo from './Remix-Logo.png';
 import Gallery from './Gallery';
 import './Gallery.css';
 import UserRemixGallery from './UserRemixGallery';
-
+import Loginregisterpage from './Loginregisterpage';
+import Sendemail from './Sendemail';
+// import downloadimage from './one.jpg'
 
 class App extends Component {
 
@@ -16,19 +18,25 @@ class App extends Component {
         <header className="App-header">
           <img src={remixLogo} className="App-logo" alt="logo" />
           <div className="navbar">
-              <Link to="/">Gallery</Link>
+              <Link to="/Gallery">Gallery</Link>
               <Link to="/UserRemixGallery">Remixes</Link>
-
+              <Link to="/Loginregisterpage">Login / Register</Link>
+              <Link to="/Sendemail">Email</Link>
+          </div>
+          <div className="routes">
               <Switch>
                 <Route exact path="/Gallery" component={Gallery} />
                 <Route exact path="/UserRemixGallery" component={UserRemixGallery} />
+                <Route exact path="/Loginregisterpage" component={Loginregisterpage} />
+                <Route exact path="/Sendemail" component={Sendemail} />
               </Switch>
           </div>
+          {/* <div refs='gallery-container' className='container-fluid gallery-container'>
+            <div className='row'>
+                <a href="/UserRemixGallery"><img src={downloadimage} /></a>
+            </div>
+        </div> */}
         </header>
-        <div>
-         
-          <Gallery />
-        </div>
       </div>
     );
   }
---
+++ client/src/Sendemail.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+// import Sendemail from './components/Sendemail';
+
+class Sendemailfunction extends Component {
+    constructor() {
+        super();
+        this.state = {
+            transporter: []
+        }
+    }
+    
+    componentDidMount() {
+        axios.get('/').then(({data})=>{
+        this.setState(data)
+        });
+      }
+    
+    sendemail() {
+        axios.post('/api/mail').then(({data})=>{
+        this.setState(data);
+        })
+        alert('Check your inbox!');
+    }
+    
+    render() {
+        return (
+            <div>
+                <div className="Email">
+                    <button onClick={this.sendemail.bind(this)}>Testing: Receive Email from Server</button>
+                </div>
+            </div>
+        );
+    }
+    }
+      
+export default Sendemailfunction;
+
+// componentDidMount() {
+//     $.ajax({
+//       url: this.props.url,
+//       dataType: 'json',
+//       cache: false,
+//       success: function(data) {
+//         this.setState({data: data});
+//       }.bind(this),
+//       error: function(xhr, status, err) {
+//         console.error(this.props.url, status, err.toString());
+//       }.bind(this)
+//     });
+//   }
